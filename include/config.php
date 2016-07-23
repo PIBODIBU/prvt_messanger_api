96afaf44c6941f501bf32ef35c85ece672d54309
Roman S <romeo97934@gmail.com>
2016-07-23 18:14:26 +0000

    Initial commit

---
+++ include/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'mysql_user');
+define('DB_PASSWORD', '2DKcMXUvsdx6aCTn');
+define('DB_HOST', '207.244.95.136');
+define('DB_NAME', 'gcm_chat');
+
+define("GOOGLE_API_KEY", "AIzaSyB30nP01C-UtBpN7WoeLERG2Hi4B8KL92I");
+
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
+
+?>
